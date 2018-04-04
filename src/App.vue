eafc765b48e242ff74f6ccbd98840dbce605ec5c
DeraIeS <kotowicztom@gmail.com>
2018-04-04 20:41:15 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AnimatedList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AnimatedList from './components/AnimatedList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    AnimatedList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/AnimatedList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+const HTTP = new axios.create({
+  baseURL: 'https://swapi.co/api/'
+})
+export default {
+  data: () => ({
+    people: []
+  }),
+  methods: {
+    async getPeople(page = 1) {
+      return await HTTP.get('people', {
+        page
+      })
+
+      this.people = result.data.results
+      this.$options.count = result.data.count
+      console.log(result);
+      console.log(this.people);
+      console.log(this.$options.count);
+    }
+  },
+  created() {
+    this.getPeople()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
